a64ca9e097cae2c9e6b13a48f2b54703239f89ef
Wjavier15 <98571034+Wjavier15@users.noreply.github.com>
2022-03-23 22:20:58 -0600

    Add files via upload

---
+++ RaizCudrada.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+main(){
+	
+    double a,b;
+    cout<<"ingrese un numero:";cin>>a;
+    b=sqrt(a);
+    cout<<"la raiz cuadra de "<<a<<" es:"<<b;
+    
+ return 0;
+}
